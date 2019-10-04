fafe39a52da75448d961af1897d5a821a27d85e0
Sreeramji K S <jamji1729@gmail.com>
2019-10-05 03:37:02 +0530

    Before Evaluation

--- postgres_dbms.py
+++ postgres_dbms.py
@@ -2,7 +2,7 @@ import psycopg2
 import sys
 import datetime
 import os
-
+from mailjet_rest import Client
 # Connection variables Declaration
 conn = psycopg2.connect(database='postgres', user="postgres",
                         password="postgre123", host="127.0.0.1", port="5432")
@@ -70,6 +70,7 @@ def manageLeave(leave_id, status, c):
     print(q)
     c.execute(q)
     conn.commit()
+    sendMail(leave_id, status, c)
     print("Successfully updated")
 
 
@@ -99,22 +100,72 @@ def applyleave(emp_id, start_date, end_date, l_type, reason, c):
     conn.commit()
     print("Successful Applied")
 
+
+def sendMail(leave_id, status, c):
+    cmd = 'select email_id from EMPLOYEE where emp_id in (select emp_id from emp_leave where leave_id = {})'.format(
+        leave_id)
+    c.execute(cmd)
+    mail = c.fetchall()
+    cmd = 'select name from EMPLOYEE where emp_id in (select emp_id from emp_leave where leave_id = {})'.format(
+        leave_id)
+    c.execute(cmd)
+    name = c.fetchall()
+    print(mail, name)
+    mail = str(mail[0][0])
+    name = str(name[0][0])
+    print(mail, name)
+    cmd = 'select emp_id from EMPLOYEE where emp_id in (select emp_id from emp_leave where leave_id = {})'.format(
+        leave_id)
+    c.execute(cmd)
+    emp_id = str(c.fetchall()[0][0])
+    if status == 'approve':
+        textpart = 'Your leave has been approved'
+    else:
+        textpart = 'Your leave has NOT been approved'
+    api_key = '6518650c4ef85ce7df9b9300c978ecff'
+    api_secret = 'f295ed2a1e96db7124367ae855de8ec3'
+    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+    data = {
+        'Messages': [
+            {
+                "From": {
+                    "Email": "vbharath472@gmail.com",
+                    "Name": "Bharath"
+                },
+                "To": [
+                    {
+                        "Email": mail,
+                        "Name": name
+                    }
+                ],
+                "Subject": "Regarding Leave Application",
+                "TextPart": "My first Mailjet email",
+                "HTMLPart": "<h3>Hey {name}, {textpart}. <a href='http://localhost:4000/employee/{emp_id}'>Click here to view your log</a></h3>".format(name=name,textpart=textpart,emp_id=emp_id),
+                "CustomID": "AppGettingStartedTest"
+            }
+        ]
+    }
+    result = mailjet.send.create(data=data)
+    print(result.status_code)
+    print(result.json())
+
 # Closing database connection
 
 
 def closedb():
-	if(conn):
-	    c.close()
-	    conn.close()
-	print("\nPostgreSQL connection is closed")
-	sys.exit()
+    if(conn):
+        c.close()
+        conn.close()
+    print("\nPostgreSQL connection is closed")
+    sys.exit()
 
 
 def viewTable(c, table_name):
-	cmd = '''  select * from {}  '''.format(table_name)
-	c.execute(cmd)
-	tab = c.fetchall()
-	for row in tab: print(row)
+    cmd = '''  select * from {}  '''.format(table_name)
+    c.execute(cmd)
+    tab = c.fetchall()
+    for row in tab:
+        print(row)
 
 
 def executeFile(fn, c, conn):
@@ -125,26 +176,28 @@ def executeFile(fn, c, conn):
         try:
             c.execute(cmd)
         except Exception as exc:
-            c,conn = reconnectDB(c,conn)
+            c, conn = reconnectDB(c, conn)
             print(cmd)
             print(exc)
         print(cmd)
-    return c,conn
+    return c, conn
 
-def reconnectDB(c,conn):
-	conn.commit()
-	conn.close()
-	conn = psycopg2.connect(database='postgres', user="postgres",
-                        password="postgre123", host="127.0.0.1", port="5432")
-	c = conn.cursor()
-	return c,conn
 
-def refreshDB(c,conn):
-    c,conn  = executeFile('dropper_db.sql' , c, conn)
-    c,conn = reconnectDB(c,conn)
-    c,conn = executeFile('creater_db.sql' , c, conn)
-    c,conn = reconnectDB(c,conn)
-    c,conn = executeFile('inserter_db.sql', c, conn)
-    c,conn = reconnectDB(c,conn)
+def reconnectDB(c, conn):
+    conn.commit()
+    conn.close()
+    conn = psycopg2.connect(database='postgres', user="postgres",
+                            password="postgre123", host="127.0.0.1", port="5432")
+    c = conn.cursor()
+    return c, conn
+
+
+def refreshDB(c, conn):
+    c, conn = executeFile('dropper_db.sql', c, conn)
+    c, conn = reconnectDB(c, conn)
+    c, conn = executeFile('creater_db.sql', c, conn)
+    c, conn = reconnectDB(c, conn)
+    c, conn = executeFile('inserter_db.sql', c, conn)
+    c, conn = reconnectDB(c, conn)
     print("Database restarted")
-    return c,conn
+    return c, conn
